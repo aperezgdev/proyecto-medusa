a3746ec9f594d46865ea28cbb6d7954f955ef437
Alex Perez <aperezgdev@gmail.com>
2023-01-31 23:58:26 +0100

    Añadido el aggregate root y implementado metodos del propio

--- src/Ingreso/domain/Ingreso.ts
+++ src/Ingreso/domain/Ingreso.ts
@@ -1,17 +1,20 @@
 import { type Concepto } from '../../Concepto/domain/Concepto'
 import { type Cuenta } from '../../Cuenta/domain/Cuenta'
+import { type CuentaCantidad } from '../../Cuenta/domain/CuentaCantidad'
+import { AggregateRoot } from '../../Shared/domian/AggregateRoot'
 import { Uuid } from '../../Shared/Uuid'
 import { type User } from '../../User/domain/User'
 import { type IngresoCantidad } from './IngresoCantidad'
 import { type IngresoFecha } from './IngresoFecha'
 import { type IngresoId } from './IngresoId'
 
-export class Ingreso {
+export class Ingreso extends AggregateRoot {
   readonly id: IngresoId
   readonly ingresoCantidad: IngresoCantidad
   readonly ingresoConcepto: Concepto
   readonly ingresoFecha: IngresoFecha
   readonly cuenta: Cuenta
+  readonly resultadoCuenta: CuentaCantidad
   readonly user: User
 
   constructor(
@@ -20,13 +23,16 @@ export class Ingreso {
     concepto: Concepto,
     fecha: IngresoFecha,
     cuenta: Cuenta,
+    resultado: CuentaCantidad,
     user: User
   ) {
+    super()
     this.id = id
     this.ingresoCantidad = cantidad
     this.ingresoConcepto = concepto
     this.ingresoFecha = fecha
     this.cuenta = cuenta
+    this.resultadoCuenta = resultado
     this.user = user
   }
 
@@ -35,8 +41,21 @@ export class Ingreso {
     concepto: Concepto,
     fecha: IngresoFecha,
     cuenta: Cuenta,
+    resultado: CuentaCantidad,
     user: User
   ) {
-    return new Ingreso(Uuid.random(), cantidad, concepto, fecha, cuenta, user)
+    return new Ingreso(Uuid.random(), cantidad, concepto, fecha, cuenta, resultado, user)
+  }
+
+  toPrimitives() {
+    return {
+      id: this.id,
+      cantidad: this.ingresoCantidad.value,
+      concepto: this.ingresoConcepto.id,
+      fecha: this.ingresoFecha.value,
+      cuenta: this.cuenta.id.value,
+      resultado: this.resultadoCuenta.value,
+      user: this.user.id
+    }
   }
 }
