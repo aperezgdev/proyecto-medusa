199b590f6c6804a30e28746dd7fff386afa55777
marioesturo <marioesturo@gmail.com>
2023-01-31 14:08:28 +0100

    Cambios config

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/apps/frontend/index.html',
+    './src/apps/frontend/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
