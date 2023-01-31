8974ccb9be4b1ec217ab04a6c8d2830a8783023a
Alex Perez <aperezgdev@gmail.com>
2023-01-31 23:58:47 +0100

    creada clase cuenta

--- src/Cuenta/domain/Cuenta.ts
+++ src/Cuenta/domain/Cuenta.ts
@@ -1,2 +1,35 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class Cuenta {}
+import { type IngresoCantidad } from '../../Ingreso/domain/IngresoCantidad'
+import { AggregateRoot } from '../../Shared/domian/AggregateRoot'
+import { Uuid } from '../../Shared/Uuid'
+import { type User } from '../../User/domain/User'
+import { type CuentaCantidad } from './CuentaCantidad'
+import { type CuentaId } from './CuentaId'
+
+export class Cuenta extends AggregateRoot {
+  readonly id: CuentaId
+  private cuentaCantidad: CuentaCantidad
+  readonly user: User
+
+  constructor(id: CuentaId, cuentaCantidad: CuentaCantidad, user: User) {
+    super()
+    this.id = id
+    this.cuentaCantidad = cuentaCantidad
+    this.user = user
+  }
+
+  static create(cantidad: CuentaCantidad, user: User) {
+    return new Cuenta(Uuid.random(), cantidad, user)
+  }
+
+  ingresar(ingresoCantidad: IngresoCantidad) {
+    this.cuentaCantidad = this.cuentaCantidad.ingresar(ingresoCantidad.value)
+  }
+
+  toPrimitives() {
+    return {
+      id: this.id.value,
+      cantidad: this.cuentaCantidad.value,
+      user: this.user.id.value
+    }
+  }
+}
