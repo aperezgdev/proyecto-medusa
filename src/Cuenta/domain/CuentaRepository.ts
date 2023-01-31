1e02d025ad0a30aeac6c443bfd4d44ba423e64b3
Alex Perez <aperezgdev@gmail.com>
2023-01-31 23:59:54 +0100

    repositorio para cuenta

--- src/Cuenta/domain/CuentaRepository.ts
+++ src/Cuenta/domain/CuentaRepository.ts
@@ -2,5 +2,6 @@ import { type Cuenta } from './Cuenta'
 import { type CuentaId } from './CuentaId'
 
 export interface CuentaRepository {
+  save: (cuenta: Cuenta) => Promise<void>
   findById: (cuentaId: CuentaId) => Promise<Cuenta>
 }
