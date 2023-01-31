9f4ea5ad973067764a12f79b313421478a746924
Alex Perez <aperezgdev@gmail.com>
2023-01-31 23:59:45 +0100

    clase concepto creada

--- src/Concepto/domain/Concepto.ts
+++ src/Concepto/domain/Concepto.ts
@@ -1,2 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class Concepto {}
+import { AggregateRoot } from '../../Shared/domian/AggregateRoot'
+import { type ConceptoId } from './ConceptoId'
+import { type ConceptoNombre } from './ConceptoNombre'
+
+export class Concepto extends AggregateRoot {
+  readonly id: ConceptoId
+  readonly conceptoNombre: ConceptoNombre
+
+  constructor(id: ConceptoId, nombre: ConceptoNombre) {
+    super()
+    this.id = id
+    this.conceptoNombre = nombre
+  }
+
+  toPrimitives() {
+    return {
+      id: this.id,
+      conceptoNombre: this.conceptoNombre
+    }
+  }
+}
