7e76462542c43db594c01199ac533ee8e1bb3ad3
Alex Perez <aperezgdev@gmail.com>
2023-02-02 22:12:34 +0100

    refactorizado opciones fetch

--- src/apps/frontend/service/registerUser.ts
+++ src/apps/frontend/service/registerUser.ts
@@ -1,12 +1,15 @@
 import { type User } from '../interfaces/User'
 
+const FETCH_OPTIONS = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
 export const registerUser = async (newUser: User) => {
   await fetch('http://localhost:3001/user', {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    ...FETCH_OPTIONS,
     body: JSON.stringify(newUser)
   }).catch((err) => {
     console.error(err)
