2103a75dfc1c4068202529d6487e8188cbceaa9f
marioesturo <marioesturo@gmail.com>
2023-02-03 00:28:42 +0100

    Interfaz Ventana de Ingresos

--- src/apps/frontend/App.tsx
+++ src/apps/frontend/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Movimientos } from './pages/Movimientos'
 import { Registro } from './pages/Registro'
+import { Ingresos } from './pages/Ingresos'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/movimientos" element={<Movimientos />} />
+        <Route path="/ingresos" element={<Ingresos />} />
       </Routes>
     </BrowserRouter>
   )
