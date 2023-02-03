69fe04fb96a825ffa94ec077f3b6218e57e35d1c
Alex Perez <aperezgdev@gmail.com>
2023-02-03 19:30:39 +0100

    cambiado el userauthcriteria

---
+++ src/User/domain/UserUsernameCriteria.ts
@@ -0,0 +1,13 @@
+import { Criteria } from '../../Shared/domian/Criteria/Criteria.js'
+import { Filter } from '../../Shared/domian/Criteria/Filter.js'
+import { FilterOperator } from '../../Shared/domian/Criteria/FilterOperator.js'
+import { Order } from '../../Shared/domian/Criteria/Order.js'
+import { OrderType } from '../../Shared/domian/Criteria/OrderType.js'
+
+export class UserUsernameCriteria extends Criteria {
+  constructor(usuario: string) {
+    super([
+      new Filter('usuario', FilterOperator.EQUAL, usuario)
+    ], new Order('usuario', OrderType.NONE))
+  }
+}
