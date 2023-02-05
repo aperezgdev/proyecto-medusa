5c55f5c8b2622a7493a71c8ab9dd038db0aa75c5
Alex Perez <aperezgdev@gmail.com>
2023-02-05 17:23:54 +0100

    auth middleware jwt

---
+++ src/apps/backend/middleware/auth.ts
@@ -0,0 +1,29 @@
+import { type Response, type Request, type NextFunction } from 'express'
+import jwt, { type Secret } from 'jsonwebtoken'
+
+interface Auth {
+  id: string
+}
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+  const authorization = req.get('authorization')
+
+  if (!authorization) {
+    res.status(400).send()
+    return
+  }
+
+  const JWT_SECRET: Secret = process.env.JWT_SECRET ?? 'xd'
+  const token = authorization.substring(7)
+  const decodeToken = jwt.verify(token, JWT_SECRET)
+
+  if (!decodeToken) {
+    res.status(401).send()
+    return
+  }
+
+  const auth = decodeToken as Auth
+  res.locals.id = auth.id
+
+  next()
+}
