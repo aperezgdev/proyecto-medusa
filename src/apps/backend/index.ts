b28f8c48019e4d50c2c98e7ed957884f1ece4c5d
Alex Perez <aperezgdev@gmail.com>
2023-02-08 15:53:00 +0100

    Cambiada la configuración del cors

--- src/apps/backend/index.ts
+++ src/apps/backend/index.ts
@@ -1,9 +1,11 @@
+import 'dotenv'
 import 'reflect-metadata'
 import './dependency-injection/index.js'
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import { routes } from './routes/index.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 const PORT = 3001
@@ -11,7 +13,11 @@ const PORT = 3001
 const jsonParser = bodyParser.json()
 
 app.use(jsonParser)
-app.use(cors())
+app.use(cors({
+  origin: true,
+  credentials: true
+}))
+app.use(cookieParser())
 app.use(routes)
 
 app.listen(PORT, () => {
