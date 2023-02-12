47207b1cc55dca019c1986dede9f1c0ae139f759
Alex Perez <aperezgdev@gmail.com>
2023-02-12 20:50:54 +0100

    cambiado getIngresos al ingresos service

--- src/apps/frontend/service/IngresoService.ts
+++ src/apps/frontend/service/IngresoService.ts
@@ -1,4 +1,4 @@
-export const eliminarIngreso = async (idIngreso: string, token: string) => {
+export const deleteIngreso = async (idIngreso: string, token: string) => {
   const FETCH_OPTIONS = {
     method: 'DELETE',
     headers: {
@@ -7,10 +7,26 @@ export const eliminarIngreso = async (idIngreso: string, token: string) => {
     }
   }
 
-  await fetch('http://localhost:3001/ingresos', {
+  return await fetch('http://localhost:3001/ingresos', {
     ...FETCH_OPTIONS,
     body: JSON.stringify({ idIngreso })
   }).catch((err) => {
     console.error(err)
   })
+}
+
+export const getIngresos = async (token: string) => {
+  const FETCH_OPTIONS = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  const result = await fetch('http://localhost:3001/ingresos/', {
+    ...FETCH_OPTIONS
+  })
+
+  return await result.json()
 }
